8b7d110a6cbf408301340487bee7e66eba30d2b6
Carlos <timmy.cg18@gmail.com>
2022-03-01 19:42:52 +0100

    Añadimos los colores personalizados a tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      'azul-principal': '#83C9F4',
+      'azul-normal': '#A3D5FF',
+      'azul-claro': '#D9F0FF',
+      'fondo': '#edf6fa',
+    },
   },
   plugins: [],
 }
