840d2c2c7ca2e7275533844f899202ee06630119
Carlos <timmy.cg18@gmail.com>
2022-03-02 12:03:21 +0100

    Crear el navBar para implementarlo en las paginas

---
+++ resources/views/index/nav.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    {{-- Favicon de la pagina --}}
+    <link rel="icon" type="image/x-icon" href="http://localhost/grind-skateshop/resources/img/favicon.png">
+    
+    {{-- Custom CSS Tailwind --}}
+    <link rel="stylesheet" href="css/app.css">
+    
+    <title>Grind SkateShop</title>
+
+</head>
+<body class="bg-fondo">
+    <div class="w-full flex justify-between py-6 px-6 bg-azul-principal items-center">
+        <a href="">
+            <h1 class="font-bold text-3xl grid">
+                <span>Grind</span>
+                <span>SkateShop</span>
+            </h1>
+        </a>
+
+        <div class="flex justify-between text-lg">
+            <a class="mx-6" href="#">Tablas</a>
+            <a class="mx-6" href="#">Ejes</a>
+            <a class="mx-6" href="#">Ruedas</a>
+            <a class="mx-6" href="#">Marcas</a>
+            <a class="mx-6" href="#">Sobre Nosotros</a>
+        </div>
+
+        <div class="flex text-lg">
+            <a href="#">Login</a>
+        </div>
+    </div>
