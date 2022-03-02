8dfb1cb9611f05619a8fd2b217bc4f67864e5ed6
Carlos <timmy.cg18@gmail.com>
2022-03-02 17:36:32 +0100

    Correcion de colores personalizados

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,13 @@ module.exports = {
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
-    colors: {
-      'azul-principal': '#83C9F4',
-      'azul-normal': '#A3D5FF',
-      'azul-claro': '#D9F0FF',
-      'fondo': '#edf6fa',
+    extend: {
+      colors: {
+        'azul-principal': '#83C9F4',
+        'azul-normal': '#A3D5FF',
+        'azul-claro': '#D9F0FF',
+        'fondo': '#edf6fa',
+      },
     },
   },
   plugins: [],
