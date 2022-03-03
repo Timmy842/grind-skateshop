d1f7a19c0ea4015d537b63aabc850fafca843632
Carlos <timmy.cg18@gmail.com>
2022-03-03 21:24:21 +0100

    Creada la tabla productos

---
+++ database/migrations/2022_03_03_201723_create_productos_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+
+            $table->bigInteger('marca_id')->unsigned();
+            $table->bigInteger('tipo_id')->unsigned();
+
+            $table->string('nombre', 100);
+            $table->string('descripcion', 500);
+            $table->string('image', 500);
+            $table->decimal('precio', 10, 2);
+            $table->string('medida', 10);
+            $table->string('tipo_producto', 100);
+            
+            $table->timestamps();
+
+            $table->foreign('marca_id')->references('id')->on('marcas')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('tipo_id')->references('id')->on('marcas')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+};
