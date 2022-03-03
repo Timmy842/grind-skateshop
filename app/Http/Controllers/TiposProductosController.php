4c170a8d94ac728430dbc137338fbd737c1547c8
Carlos <timmy.cg18@gmail.com>
2022-03-03 21:15:49 +0100

    Creacion del controlador y modelo de tipo_producto

---
+++ app/Http/Controllers/TiposProductosController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TiposProductos;
+use Illuminate\Http\Request;
+
+class TiposProductosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\TiposProductos  $tiposProductos
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TiposProductos $tiposProductos)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\TiposProductos  $tiposProductos
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TiposProductos $tiposProductos)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\TiposProductos  $tiposProductos
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TiposProductos $tiposProductos)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\TiposProductos  $tiposProductos
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TiposProductos $tiposProductos)
+    {
+        //
+    }
+}
