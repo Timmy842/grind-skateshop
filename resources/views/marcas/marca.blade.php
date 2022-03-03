2d1619e0b1f5d4329e21411b8248ac6334243ca4
Carlos <timmy.cg18@gmail.com>
2022-03-03 23:17:27 +0100

    Edicion de la vista de marca.blade.php

--- resources/views/marcas/marca.blade.php
+++ resources/views/marcas/marca.blade.php
@@ -3,9 +3,9 @@
 
 <h2 class="flex justify-center py-8 text-2xl font-bold">Grind SkateShop, la tienda para skaters</h2>
 
-<div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
+<div class="flex justify-center">
     @foreach ($marcas as $marca)
-    <div class="max-w-sm m-8 bg-white rounded-lg shadow-md hover:m-2">
+    <div class="w-3/4 m-8 bg-white rounded-lg shadow-md">
         <div class="flex justify-center h-60">
             <img class="p-8 rounded-t-lg" src="{{ $marca->image }}" alt="product image" height="180px">
         </div>
@@ -22,5 +22,7 @@
     </div>
     @endforeach
 </div>
+
+<h2 class="flex justify-center py-8 text-2xl font-bold">Productos de la Marca</h2>
 
 @endsection
