4def82f7a838c0e9ebbec6dc2e118349bcd486c8
Carlos <timmy.cg18@gmail.com>
2022-03-08 11:28:15 +0100

    Añadida la descripcion y modificado los estilos para el responsive

--- resources/views/productos/tabla.blade.php
+++ resources/views/productos/tabla.blade.php
@@ -1,21 +1,21 @@
 @extends('layouts.header')
 @section('content')
 
-<h2 class="flex justify-center py-8 text-2xl font-bold">Grind SkateShop, la tienda para skaters</h2>
+<h2 class="py-8 text-2xl font-bold text-center">Grind SkateShop, la tienda para skaters</h2>
 
 <div class="flex justify-center">
     @foreach ($productos as $producto)
     <div class="w-3/4 p-8 m-8 bg-white rounded-lg shadow-md">
-        <div class="grid grid-cols-3 gap-12">
-            <div class="flex justify-center h-60">
+        <div class="grid grid-cols-1 gap-12 md:grid-cols-3">
+            <div class="md:flex md:justify-center h-60">
                 <img class="p-8 rounded-t-lg" src="{{ $producto->image }}" alt="product image" height="180px">
             </div>
 
-            <div class="col-start-2 col-end-4">
+            <div class="md:col-start-2 md:col-end-4">
                 <h2 class="mb-8 text-5xl font-bold tracking-tight text-center text-gray-900">
                     {{ $producto->nombre }}
                 </h2>
-                
+
                 <div class="items-start font-medium">
                     <p class="text-xl">Medida: {{ $producto->medida }}</p>
                 </div>
@@ -24,9 +24,9 @@
                     <span class="text-3xl font-bold text-azul-principal">{{ $producto->precio }}€</span>
                 </div>
 
-                <div class="grid items-center grid-cols-3 gap-4 my-4">
-                    <div class="col-start-1">
-                        <div class="relative flex flex-row w-full h-10 mt-1 bg-transparent rounded-lg">
+                <div class="items-center my-4 md:grid md:grid-cols-3 md:gap-4">
+                    <div class="md:col-start-1">
+                        <div class="relative flex flex-row h-10 mt-1 bg-transparent rounded-lg w md:w-full">
                             <button data-action="decrement" class="w-20 h-full text-gray-600 bg-gray-300 rounded-l outline-none cursor-pointer hover:text-gray-700 hover:bg-gray-400">
                                 <span class="m-auto text-2xl font-thin">−</span>
                             </button>
@@ -47,13 +47,22 @@
 
                 <div class="flex items-center justify-around">
                     <h2 class="text-2xl font-bold text-azul-principal">Marca:</h2>
-                    
+
                     <a href="{{ url('/marca.' . $marcas[0]->id) }}">
                         <img src="{{ $marcas[0]->image }}" alt="{{ $marcas[0]->nombre }}">
                     </a>
                 </div>
             </div>
         </div>
+
+        <hr class="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+
+        <div class="p-4 rounded-lg bg-fondo">
+            <h2 class="mb-4">Descripcion del Producto:</h2>
+            
+            <p class="text-azul-principal">{{ $producto->descripcion }}</p>
+        </div>
+
     </div>
     @endforeach
 </div>
