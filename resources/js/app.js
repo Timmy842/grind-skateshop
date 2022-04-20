8576345bd3e17bc08d8c6976f0148cc9fa2ba673
Carlos <timmy.cg18@gmail.com>
2022-04-20 23:37:22 +0200

    Añadido los require al app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
+const secret_id = document.getElementById('secret_id');
+
 require('./bootstrap');
+require('./cantidadProducto');
+require('./header');
+
+if (secret_id !== null)
+{
+    require('./profileFetchPetition');   
+}
