b4d0b0c8578b9c86d8a68deebec3a2a0bbeab5ba
Carlos <timmy.cg18@gmail.com>
2022-05-17 20:02:12 +0200

    cambio de espacio en balnco

--- resources/js/header.js
+++ resources/js/header.js
@@ -3,6 +3,7 @@ let buttonLogout = document.querySelector('#logout');
 if(buttonLogout === null)
 {
     console.log('Waiting...');
+    
 } else {
 
     buttonLogout.addEventListener('click', function (e) {
